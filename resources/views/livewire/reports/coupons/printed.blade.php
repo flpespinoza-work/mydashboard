218574a61dbddfee86aa91d4141e9af82ff66b5f
Felipe Espinoza <flpespinoza@gmail.com>
2021-09-06 06:52:27 -0500

    filtros en vista de reportes

--- resources/views/livewire/reports/coupons/printed.blade.php
+++ resources/views/livewire/reports/coupons/printed.blade.php
@@ -1,9 +1,9 @@
 <div>
-    <div class="p-3 rounded-md shadow-sm bg-gray-25">
-        <form class="items-end justify-end w-full md:flex" wire:submit.prevent="generateReport">
+    <div class="px-2 py-3 bg-white rounded-md">
+        <form class="items-end justify-start w-full md:flex" wire:submit.prevent="generateReport">
             <div class="px-2 space-y-2 md:w-1/3">
                 <label for="store" class="block text-sm font-medium text-gray-700">Selecione un establecimiento</label>
-                <select wire:model.defer="filters.store" id="store" name="store" autocomplete="store" class="block w-full p-3 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <select wire:model.defer="filters.store" id="store" name="store" autocomplete="store" class="block w-full p-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     <option></option>
                     @foreach ($stores as $store => $name)
                     <option value="{{ $store }}">{{ $name }}</option>
@@ -15,13 +15,13 @@
                 <label for="store" class="block text-sm font-medium text-gray-700">Seleccione la fecha o periodo</label>
                 <div class="relative">
                     <x-heroicon-s-calendar class="absolute w-5 h-5 text-gray-400 top-2 left-2"/>
-                    <input type="text" id="date" class="block w-full py-3 pl-10 mt-1 bg-gray-100 border-gray-300 rounded-md shadow-sm sm:text-sm">
+                    <input type="text" id="date" readonly class="block w-full pl-10 mt-1 border-gray-300 rounded-md shadow-sm bg-gray-25 sm:text-sm">
                 </div>
             </div>
 
-            <div class="px-2 md:w-1/3">
-                <button type="submit" class="flex items-center w-full px-5 py-3 text-sm font-semibold rounded-md bg-orange text-gray-50">
-                    <span>Generar reporte</span>
+            <div class="px-2">
+                <button type="submit" class="flex items-center px-5 py-2 text-sm font-semibold rounded-md bg-orange text-gray-50">
+                    Generar reporte
                 </button>
             </div>
         </form>
@@ -120,6 +120,7 @@
             },
             "showCustomRangeLabel": false,
             "alwaysShowCalendars": true,
+            "autoApply": true,
             "startDate": moment(),
             "endDate": moment()
         });
