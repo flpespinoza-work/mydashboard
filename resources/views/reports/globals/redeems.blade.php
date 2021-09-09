6b94b72df3c287ec81a6f266b1b8cb984a8e7db7
Felipe Espinoza <flpespinoza@gmail.com>
2021-09-08 19:44:26 -0500

    vistas para reportes de usuarios, ventas, globales y saldos

---
+++ resources/views/reports/globals/redeems.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="module">Reportes</x-slot>
+    <x-slot name="title">
+        <div class="flex items-center">
+            <div class="p-2 bg-gray-800 rounded-full">
+                <x-heroicon-s-tag class="w-3 h-3 text-gray-50"/>
+            </div>
+            <span class="ml-2 text-gray-800">Reporte global de canjes diarios</span>
+        </div>
+    </x-slot>
+    <x-slot name="description"></x-slot>
+    <livewire:reports.globals.redeems/>
+</x-app-layout>
---
+++ resources/views/reports/users/history.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="module">Reportes</x-slot>
+    <x-slot name="title">
+        <div class="flex items-center">
+            <div class="p-2 bg-gray-800 rounded-full">
+                <x-heroicon-s-tag class="w-3 h-3 text-gray-50"/>
+            </div>
+            <span class="ml-2 text-gray-800">Reporte acumulado de usuarios</span>
+        </div>
+    </x-slot>
+    <x-slot name="description">Acumulado de usuarios por establecimiento</x-slot>
+    <livewire:reports.users.history/>
+</x-app-layout>
