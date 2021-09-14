c3c282b6f54016daf1bb47b2b4de80cd3fea21b5
Felipe Espinoza <felipe.espinoza@tokencash.mx>
2021-09-14 11:07:21 -0500

    cambio de icono en titulo de reportes

--- resources/views/reports/sales/history.blade.php
+++ resources/views/reports/sales/history.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="title">
         <div class="flex items-center">
             <div class="p-2 bg-gray-800 rounded-full">
-                <x-heroicon-s-tag class="w-3 h-3 text-gray-50"/>
+                <x-heroicon-s-currency-dollar class="w-3 h-3 text-gray-50"/>
             </div>
             <span class="ml-2 text-gray-800">Reporte acumulado de ventas</span>
         </div>
