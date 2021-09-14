47fbe0480b7e1859fd8630e82aee804ddd2776d4
Felipe <felipe.espinoza@tokencash.mx>
2021-09-14 08:40:57 -0500

    cambio en configuracion de url

--- config/app.php
+++ config/app.php
@@ -52,9 +52,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://dashboard.tokencash.mx'),
+    'url' => env('APP_URL', 'http://new.dashboard.tokencash.mx'),
 
-    'asset_url' => env('ASSET_URL', 'https://dashboard.tokencash.mx'),
+    'asset_url' => env('ASSET_URL', 'http://new.dashboard.tokencash.mx'),
 
     /*
     |--------------------------------------------------------------------------
