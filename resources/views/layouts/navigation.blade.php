fe609680b60305ff4cdac1ade3f6f5757c8161e1
Felipe Espinoza <felipe.espinoza@tokencash.mx>
2021-09-28 14:55:43 -0500

    posicion del boton de cerrar sesion siempre al final

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <sidebar :class="{ '-translate-x-72': !sidebarOpen }" id="sidebar"
-class="absolute top-0 left-0 z-40 flex-shrink-0 inline-block h-screen max-h-full py-4 overflow-y-scroll transition-transform duration-200 ease-in-out transform bg-white border-r border-gray-150 w-72 lg:static lg:left-auto lg:top-auto lg:translate-x-0 lg:overflow-y-auto no-scrollbar">
+class="absolute top-0 left-0 z-40 flex flex-col flex-shrink-0 h-screen max-h-full py-4 overflow-y-scroll transition-transform duration-200 ease-in-out transform bg-white border-r border-gray-150 w-72 lg:static lg:left-auto lg:top-auto lg:translate-x-0 lg:overflow-y-auto no-scrollbar">
     <div class="flex items-center px-6 pr-3 mb-10">
         <button class="mr-4 text-gray-600 outline-none lg:hidden"
         aria-controls="sidebar" aria-expanded="false" @click="sidebarOpen = !sidebarOpen">
@@ -17,7 +17,7 @@ class="absolute top-0 left-0 z-40 flex-shrink-0 inline-block h-screen max-h-full
             @include('components.menugroup', [ 'menuGroup' => $menuGroup ])
         @endforeach
     </div>
-    <div class="p-6 mt-10">
+    <div class="px-6 mt-auto">
         <form method="POST" action="{{ route('logout') }}" role="none" >
             @csrf
             <button type="submit" class="block w-full px-4 py-2 text-xs font-semibold text-center text-gray-600 bg-gray-100 rounded-md"
